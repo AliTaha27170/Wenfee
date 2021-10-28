876fda28d72e194d7109987adb28ac8295298f2c
Adham <Adham-461@hotmail.com>
2021-10-28 16:59:33 +0300

    8

--- public/js/passwords.js
+++ public/js/passwords.js
@@ -2,7 +2,7 @@ var newPassword = document.querySelector("#new_password")
 var confirmPassword = document.querySelector("#confirm_password")
 document.forms[3].onsubmit = function(e)
 {
-    if (newPassword.value !== confirmPassword.value || newPassword.value === "") {
+    if (newPassword.value !== confirmPassword.value || newPassword.value === "" || newPassword.value.length < 8 ) {
         e.preventDefault();
         newPassword.style = "border : 1px solid #f00"
         confirmPassword.style = "border : 1px solid #f00"
